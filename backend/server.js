7273ba415a14f55e8705dcf85bad0d048d61c57a
saketh169 <sakethrupa123@gmail.com>
2026-01-23 10:26:41 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5200;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // Create transporter
 const transporter = nodemailer.createTransport({
